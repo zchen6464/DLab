b111e48996f7b24f9e660ae821e8787fd0fee51f
Z Chen <zchen6464@bths.edu>
2018-12-12 10:25:30 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args)
+  {
+    Digits digits1 = new Digits(7);
+    System.out.println(digits1.isStricklyIncreasing());
+    
+    Digits digits2 = new Digits(65310);
+    System.out.println(digits2.isStricklyIncreasing());
+    
+    System.out.println(new Digits(1356).isStricklyIncreasing());
+    System.out.println(new Digits(1336).isStricklyIncreasing());
+    System.out.println(new Digits(1536).isStricklyIncreasing());
+    
+    System.out.println(digits2.getDigitList());
+  }
 }
