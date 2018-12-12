b124ac3f675eec13693c83938e5ebc2e39992d52
Z Chen <zchen6464@bths.edu>
2018-12-12 10:24:15 -0500

    Create Digits.java

---
+++ src/com/company/Digits.java
@@ -0,0 +1,25 @@
+public class Digits
+{
+  public Digits(int num)
+  {
+    digitList = new ArrayList<Integer>();
+    String x = String.valueOf(num);
+    for(int i = 0; i < x. length(); i ++)
+    {
+      digitList.add(Integer.parseInt(x.charAt(i));
+    }
+  }
+  
+  public boolean isStrictlyIncreasing()
+  {
+    for(int i = 1; i < digitList.size(); i++)
+    {
+      if(digitList.get(i-1)>= digitList.get(i))
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+© 2018 GitHub, Inc.
